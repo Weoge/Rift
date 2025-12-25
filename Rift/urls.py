92b1b70d861c281260a9f7d42c677bb9c42f8ff6
Weoge <ad.weoge@gmail.com>
2025-12-25 19:39:09 +0300

    fixed service-worker.js

--- Rift/urls.py
+++ Rift/urls.py
@@ -4,12 +4,17 @@ from django.urls import include
 from Rift.settings import DEBUG
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
+from django.conf import settings
+import os
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('app/', include('chats.urls', namespace="chats")),
     path('auth/', include('authentication.urls', namespace="authentication")),
     path('', include('main.urls', namespace="main")),
+    path('service_worker.js', serve, {'path': 'service_worker.js', 'document_root': settings.BASE_DIR}),
+    path('manifest.json', serve, {'path': 'manifest.json', 'document_root': settings.BASE_DIR}),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
