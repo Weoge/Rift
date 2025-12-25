02e50c2ed50669091a16d19925d5bbe88255249a
Weoge <ad.weoge@gmail.com>
2025-12-25 19:42:51 +0300

    fixed service-worker.js again

--- Rift/urls.py
+++ Rift/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     path('app/', include('chats.urls', namespace="chats")),
     path('auth/', include('authentication.urls', namespace="authentication")),
     path('', include('main.urls', namespace="main")),
-    path('service_worker.js', serve, {'path': 'service_worker.js', 'document_root': settings.BASE_DIR}),
+    path('service-worker.js', serve, {'path': 'service-worker.js', 'document_root': settings.BASE_DIR}),
     path('manifest.json', serve, {'path': 'manifest.json', 'document_root': settings.BASE_DIR}),
 ]
 
